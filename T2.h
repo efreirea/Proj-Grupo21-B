0ae7370c48ad2eb898ed4e8a22526eba00e01f32
efreirea <ericfa@usp.br>
2015-12-06 07:58:21 -0200

    atualizando README e corrigindo arquivos de suporte

--- T2.h
+++ T2.h
@@ -3,8 +3,8 @@
 
 //Definicao dos nomes dos executaveis gerados apos a compilacao.
 //necessario pois a funcao buildCommandLine em analyzer usa tal informacao
-#define PARALLEL_NAME "./parallel/parallel_MPI"
-#define PARALLEL_NAME_CUDA "./parallel/parallel_CUDA"
-#define SERIAL_NAME "./sequential/sequential"
+#define PARALLEL_NAME "./T2/parallel/parallel_MPI"
+#define PARALLEL_NAME_CUDA "./Proj/parallel/parallel_CUDA"
+#define SERIAL_NAME "./T2/sequential/sequential"
 
 #endif
